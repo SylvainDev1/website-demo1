3bd35257b3c575abf2fdcb005e983c9d01b87da1
SylvainDev1 <syvilledary@gmail.com>
2023-03-27 17:19:29 +0200

    Changes for responsiveness

--- app.js
+++ app.js
@@ -11,47 +11,58 @@ const formSent = document.querySelector(".form-sent");
 
 const fields = document.querySelectorAll(".field");
 
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
 // Nav Anchor links
 
 navAbout.addEventListener("click", () => {
-    window.scrollTo(0, 100);
+  window.scrollTo(0, 100);
 });
 
 navWorks.addEventListener("click", () => {
-    window.scrollTo(0, 685);
+  window.scrollTo(0, 685);
 });
 
 navPrice.addEventListener("click", () => {
-    window.scrollTo(0, 1700);
+  window.scrollTo(0, 1700);
 });
+
+//Respo nav
+function classToggle() {
+  const navs = document.querySelectorAll(".item");
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+}
+document
+  .querySelector(".navbar-link-toggle")
+  .addEventListener("click", classToggle);
 
 // Form Modal
 
 // ---Activate + reset fields
 contactBtn.addEventListener("click", () => {
-    modalbg.classList.add("modalActive");
-    fields.forEach(function(e){
-        e.value = ""
-     });
-})
+  modalbg.classList.add("modalActive");
+  fields.forEach(function (e) {
+    e.value = "";
+  });
+});
 
 // ---Close
 modalCloseBtn.addEventListener("click", () => {
-    modalbg.classList.remove("modalActive");
-})
+  modalbg.classList.remove("modalActive");
+});
 
 // ---Sent: hide form, show thank you screen
 submitBtn.addEventListener("click", () => {
-    modal.classList.add("modal-submitted");
-    formSent.classList.add("form-sent-active");
-})
+  modal.classList.add("modal-submitted");
+  formSent.classList.add("form-sent-active");
+});
 
 // ---timeout to close modal
 submitBtn.addEventListener("click", () => {
-    setTimeout(() => {
-        modalbg.classList.remove("modalActive");
-        formSent.classList.remove("form-sent-active");
-        modal.classList.remove("modal-submitted");
-
-    }, 2000)
-})
+  setTimeout(() => {
+    modalbg.classList.remove("modalActive");
+    formSent.classList.remove("form-sent-active");
+    modal.classList.remove("modal-submitted");
+  }, 2000);
+});
